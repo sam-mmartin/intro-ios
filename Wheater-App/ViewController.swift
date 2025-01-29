5c6583e30b8e5e0970c9702af6665cf1c7003e91
Sam Martin <smeirelesmartins@gmail.com>
2025-01-29 14:42:54 -0300

    Initial Commit

---
+++ Wheater-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheater-App
+//
+//  Created by Sam Martin on 29/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
